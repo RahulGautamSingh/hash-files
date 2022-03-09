53828c3e29cbd77f76b8b7169e261be4d1c94c5d
RahulGautamSingh <rahultesnik@gmail.com>
2022-03-09 16:39:31 +0530

    make logic more generix

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { fingerprint as fp1 } from "./lib/manager/manager1/index.js";
 import { fingerprint as fp2 } from "./lib/manager/manager2/index.js";
+import { fingerprint as fp3 } from "./lib/manager/manager3/index.js";
 
-console.log([fp1, fp2]);
+console.log(fp1);
+console.log(fp2);
+console.log(fp3);
