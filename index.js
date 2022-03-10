f50ba87b89e4efad42a7b0dba852d8c73f36e30c
RahulGautamSingh <rahultesnik@gmail.com>
2022-03-11 00:23:10 +0530

    chore: update file structure

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { fingerprint as fp1 } from "./lib/manager/manager1/index.js";
-import { fingerprint as fp2 } from "./lib/manager/manager2/index.js";
-import { fingerprint as fp3 } from "./lib/manager/manager3/index.js";
+// import { fingerprint as fp1 } from "./lib/modules/manager/manager1/index.js";
+import { fingerprint as fp2 } from "./lib/modules/manager/manager2/index.js";
+// import { fingerprint as fp3 } from "./lib/modules/manager/manager3/index.js";
 
-console.log(fp1);
+// console.log(fp1);
 console.log(fp2);
-console.log(fp3);
+// console.log(fp3);
+
