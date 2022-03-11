42d79815945bdddf92379362629727ff49cba481
RahulGautamSingh <rahultesnik@gmail.com>
2022-03-11 13:03:14 +0530

    chore: update hashFn logic

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// import { fingerprint as fp1 } from "./lib/modules/manager/manager1/index.js";
+import { fingerprint as fp1 } from "./lib/modules/manager/manager1/index.js";
 import { fingerprint as fp2 } from "./lib/modules/manager/manager2/index.js";
-// import { fingerprint as fp3 } from "./lib/modules/manager/manager3/index.js";
+import { fingerprint as fp3 } from "./lib/modules/manager/manager3/index.js";
 
-// console.log(fp1);
-console.log(fp2);
-// console.log(fp3);
-
+console.log([fp1,fp2,fp3]);
--- util/hashFn.js
+++ util/hashFn.js
@@ -1,40 +1,46 @@
 import hasha from "hasha";
+import fs from "fs";
 
 const options = {
   algorithm: "md5",
 };
 
-export async function getHash(manager, files, snapshots) {
-  let hashes = [];
-  for (const file of files) {
-    const hash = getFileHash(manager, file, "test");
-    hashes.push(hash);
+async function getFileHash(managerName, fileAddr) {
+  try {
+    const hash = await hasha.fromFile(
+      `./lib/modules/manager/${managerName}/${fileAddr}`,
+      options
+    );
+    return hash;
+  } catch (err) {
+    // logger.debug(`Couldn't resolve hash for ${managerName} -> ${fileName}`)
+    return undefined;
   }
-  if (snapshots) {
+}
+
+export async function getHash(manager) {
+  let hashes = [];
+  let files = fs.readdirSync(`./lib/modules/manager/${manager}`);
+
+  if (files.includes("__snapshots__")) {
+    const snapshots = fs.readdirSync(
+      `./lib/modules/manager/${manager}/__snapshots__`
+    );
+
     for (const snap of snapshots) {
-      const hash = getFileHash(manager, snap, "snap");
+      const hash = await getFileHash(manager, `__snapshots__/${snap}`);
       hashes.push(hash);
     }
   }
-  hashes = await Promise.all(hashes);
-  return hasha(hashes, options);
-}
 
-async function getFileHash(managerName, fileName, fileType) {
-  try {
-    let hash;
-    if (fileType === "test")
-      hash = await hasha.fromFile(
-        `./lib/modules/manager/${managerName}/${fileName}.spec.js`,
-        options
-      );
-    else
-      hash = hash = await hasha.fromFile(
-        `./lib/modules/manager/${managerName}/__snapshots__/${fileName}.spec.js.snap`,
-        options
-      );
-    return hash;
-  } catch (err) {
-    return undefined;
+  files = files.filter((fileName) => RegExp(/.*(spec.js)/).test(fileName));
+  for (const file of files) {
+    const hash = getFileHash(manager, file);
+    hashes.push(hash);
   }
+
+  hashes = hashes.filter(Boolean);
+  hashes = await Promise.all(hashes); //faster than using await in a for loop
+
+  return hasha(hashes, options);
 }
